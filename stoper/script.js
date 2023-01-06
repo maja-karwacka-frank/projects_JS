80eeec16d95abd66917bca2a2fe02a58218a7c17
Maja Karwacka-Frank <majakarwacka86@gmail.com>
2023-01-06 17:36:13 +0100

    Add colors panel

--- stoper/script.js
+++ stoper/script.js
@@ -7,7 +7,7 @@ const stopwatch = document.querySelector('.stopwatch');
 const time = document.querySelector('.time');
 const timeList = document.querySelector('.time-list');
 
-const infoBtn = document.querySelector('.info');
+const infoBtn = document.querySelector('.fa-question');
 const modalShadow = document.querySelector('.modal-shadow');
 const closeModalBtn = document.querySelector('.close');
 
@@ -16,6 +16,14 @@ let minutes = 0;
 let seconds = 0;
 
 let timesArr = [];
+
+// zmiana kolorów
+const colorBtn = document.querySelector('.fa-paint-brush');
+const colorPanel = document.querySelector('.colors');
+const colorOne = document.querySelector('.one');
+const colorTwo = document.querySelector('.two');
+const colorThree = document.querySelector('.three');
+let root = document.documentElement;
 
 const handleStart = () => {
 	clearInterval(countTime);
@@ -41,7 +49,6 @@ const handleStop = () => {
 	if (stopwatch.textContent !== '0:00') {
 		time.style.visibility = 'visible';
 		timesArr.push(stopwatch.textContent);
-		console.log(timesArr);
 	}
 
 	clearStuff();
@@ -72,6 +79,7 @@ const showHistory = () => {
 	timesArr.forEach((time) => {
 		const newTime = document.createElement('li');
 		newTime.innerHTML = `Pomiar nr ${num}: <span>${time}</span>`;
+
 		timeList.appendChild(newTime);
 		num++;
 	});
@@ -92,8 +100,29 @@ pauseBtn.addEventListener('click', handlePause);
 stopBtn.addEventListener('click', handleStop);
 resetBtn.addEventListener('click', handleReset);
 historyBtn.addEventListener('click', showHistory);
+
 infoBtn.addEventListener('click', showModal);
 closeModalBtn.addEventListener('click', showModal);
 window.addEventListener('click', (e) =>
 	e.target === modalShadow ? showModal() : false
 );
+
+// zmiana kolorów
+colorBtn.addEventListener('click', () => {
+	colorPanel.classList.toggle('show-colors');
+});
+
+colorOne.addEventListener('click', () => {
+	root.style.setProperty('--first-color', 'rgb(250, 20, 6)');
+	root.style.setProperty('--hover-color', 'rgb(209, 33, 24)');
+});
+
+colorTwo.addEventListener('click', () => {
+	root.style.setProperty('--first-color', 'rgb(6, 173, 250)');
+	root.style.setProperty('--hover-color', 'rgb(28, 145, 199)');
+});
+
+colorThree.addEventListener('click', () => {
+	root.style.setProperty('--first-color', 'rgb(0, 255, 42)');
+	root.style.setProperty('--hover-color', 'rgb(28, 209, 58)');
+});
