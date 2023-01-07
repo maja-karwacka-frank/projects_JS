1d74b911668456a76542842eed058ef969faff23
Maja Karwacka-Frank <majakarwacka86@gmail.com>
2023-01-07 12:04:53 +0100

    Add js in script

--- notes/script.js
+++ notes/script.js
@@ -2,7 +2,7 @@ const addBtn = document.querySelector('.add');
 const saveBtn = document.querySelector('.save');
 const cancelBtn = document.querySelector('.cancel');
 const deleteBtn = document.getElementsByClassName('delete-note');
-const deleteAllBtn = document.getElementsByClassName('delete-all');
+const deleteAllBtn = document.querySelector('.delete-all');
 
 const noteArea = document.querySelector('.note-area');
 const notePanel = document.querySelector('.note-panel');
@@ -25,5 +25,69 @@ const closePanel = () => {
 	category.selectedIndex = 0;
 };
 
+const addNote = () => {
+	if (
+		textarea.value !== '' &&
+		category.options[category.selectedIndex].value !== '0'
+	) {
+		createNote();
+		error.style.visibility = 'hidden';
+	} else {
+		error.style.visibility = 'visible';
+	}
+};
+
+const createNote = () => {
+	const newNote = document.createElement('div');
+	newNote.classList.add('note');
+	newNote.setAttribute('id', cardID);
+
+	newNote.innerHTML = `<div class="note-header">
+	<h3 class="note-title">${selectedValue}</h3>
+	<button class="delete-note" onclick='deleteNote(${cardID})'>
+		<i class="fas fa-times icon"></i>
+	</button>
+	</div>
+	<div class="note-body">
+	${textarea.value}
+	</div>`;
+
+	noteArea.appendChild(newNote);
+	cardID++;
+	textarea.value = '';
+	category.selectedIndex = 0;
+	notePanel.style.display = 'none';
+	checkColor(newNote);
+};
+
+const selectValue = () => {
+	selectedValue = category.options[category.selectedIndex].text;
+};
+
+const checkColor = (note) => {
+	switch (selectedValue) {
+		case 'Zakupy':
+			note.style.backgroundColor = 'rgb(72,255,0)';
+			break;
+		case 'Praca':
+			note.style.backgroundColor = 'rgb(255, 243, 0)';
+			break;
+		case 'Inne':
+			note.style.backgroundColor = 'hotpink';
+			break;
+	}
+};
+
+const deleteNote = (id) => {
+	const noteToDelete = document.getElementById(id);
+	noteArea.removeChild(noteToDelete);
+};
+
+const deleteAllNotes = () => {
+	noteArea.textContent = '';
+};
+
 addBtn.addEventListener('click', openPanel);
 cancelBtn.addEventListener('click', closePanel);
+saveBtn.addEventListener('click', addNote);
+deleteAllBtn.addEventListener('click', deleteAllNotes);
